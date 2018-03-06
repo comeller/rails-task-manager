e31ac16cdff1d677970f4a5d8022207d8977f038
come mellerio <come.mellerio@free.fr>
2018-03-06 14:35:15 +0100

    ex1 CRUD on rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#read one
+get "/tasks", to: "tasks#index"
+#Create one
+get "/tasks/new", to: "tasks#new"
+post "/tasks", to: "tasks#create"
+#read all
+get "/tasks/:id", to: "tasks#show", as: :task
+#update
+get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+patch "/tasks/:id", to: "tasks#update"
+#delete
+delete "task/:id", to: "tasks#destroy", as: :delete_task
 end
